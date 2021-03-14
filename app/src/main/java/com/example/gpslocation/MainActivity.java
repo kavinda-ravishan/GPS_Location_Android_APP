3de5c46d8a3df211aa5ce30db0f77460ad853a09
kavinda ravishan <kvnravishan@outlook.com>
2021-03-14 12:59:42 +0530

    Async Task assigned for post data.

--- app/src/main/java/com/example/gpslocation/MainActivity.java
+++ app/src/main/java/com/example/gpslocation/MainActivity.java
@@ -3,10 +3,9 @@ package com.example.gpslocation;
 import android.Manifest;
 import android.content.Intent;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,21 +24,22 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView1;
-    TextView textView2;
-    TextView textView3;
-    TextView textView4;
-    TextView textView5;
-    TextView textView6;
-    TextView textView7;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+    private TextView textView5;
+    private TextView textView6;
+    private TextView textView7;
 
-    EditText urlTxtBox;
+    private EditText urlTxtBox;
 
-    Button shareBtn;
-    Button sendBtn;
-    Button sendOnesBtn;
+    private Button shareBtn;
+    private Button sendBtn;
 
     private double lat ;
     private double lon ;
@@ -53,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
     private String res;
 
     private Location l;
-
-    private boolean isLocationSharing = false;
 
     private String serverURL;
 
@@ -138,9 +136,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             display();
-            if(isLocationSharing) {
-                textView7.setText(PostLocation(serverURL));
-            }
             handler.postDelayed(this,1000);
         }
     };
@@ -198,7 +193,6 @@ public class MainActivity extends AppCompatActivity {
 
         shareBtn = (Button) findViewById(R.id.shareBtn);
         sendBtn = (Button) findViewById(R.id.sendBtn);
-        sendOnesBtn = (Button) findViewById(R.id.sendOnesBtn);
 
 
         shareBtn.setOnClickListener(new View.OnClickListener() {
@@ -211,30 +205,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //POST req
+        runnable.run();
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isLocationSharing = !isLocationSharing;
-                if(isLocationSharing) {
-                    serverURL = urlTxtBox.getText().toString();
-                    urlTxtBox.setInputType(InputType.TYPE_NULL);
-                    sendBtn.setText("Stop");
-                }
-                else {
-                    urlTxtBox.setInputType(InputType.TYPE_CLASS_TEXT);
-                    sendBtn.setText("Start");
-                }
-            }
-        });
-        runnable.run();
-
-        sendOnesBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                serverURL = urlTxtBox.getText().toString();
-                textView7.setText(PostLocation(serverURL));
+                PostLocationTask();
             }
         });
     }
+
+    public void PostLocationTask(){
+        PostData postData = new PostData(this);
+        serverURL = urlTxtBox.getText().toString();
+        postData.execute(serverURL);
+    }
+
+    private static class PostData extends AsyncTask<String,Void,String> {
+
+        //For fix memory leaks
+        private WeakReference<MainActivity> activityWeakReference;
+        PostData(MainActivity activity){
+            activityWeakReference = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            MainActivity activity = activityWeakReference.get();
+            if(activity == null || activity.isFinishing()){
+                return;
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            MainActivity activity = activityWeakReference.get();
+            if(activity == null || activity.isFinishing()){
+                return "error";
+            }
+            return activity.PostLocation(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            MainActivity activity = activityWeakReference.get();
+            if(activity == null || activity.isFinishing()){
+                return;
+            }
+            activity.textView7.setText(s);
+        }
+    }
+
 }
